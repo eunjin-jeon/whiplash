44155b15d7cb12d9427bfb9453066eec4920a70e
Eunjin Jeon <92969048+eunjin-jeon@users.noreply.github.com>
2021-12-08 00:48:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+// ** 현재 javaScript 설명: 메일에 같이 첨부해서 보낸 js를 참고해서 조금 수정함 **
+// lpcover의 양쪽에 있는 화살표를 누르면 이미지가 돌아가며 교체되는 설정
+// 원래 이미지로 넣고자 했던 양쪽의 화살표를 galleryControls의 previous, next로 우선 대체함
+// previous, add, next가 있던 기존 레퍼런스파일에서 add부분은 없애고 양쪽으로 회전시키는 설정만 남김
+
+// *** 질문 및 개선하고 싶은 점 ***
+// 양쪽에 화살표 이미지를 누르면 이미지가 회전되게끔 수정하고싶음
+// 코드가 너무 어려워서 해석할수가 없음... 강의시간에 배웠던 것처럼 이해할 수 있는 코드가 필요함
+
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+constructor(container, items, controls) {
+  this.carouselContainer = container;
+  this.carouselControls = controls;
+  this.carouselArray = [...items];
+}
+
+// Update css classes for gallery
+updateGallery() {
+  this.carouselArray.forEach(el => {
+    el.classList.remove('gallery-item-1');
+    el.classList.remove('gallery-item-2');
+    el.classList.remove('gallery-item-3');
+    el.classList.remove('gallery-item-4');
+    el.classList.remove('gallery-item-5');
+  });
+
+  this.carouselArray.slice(0, 5).forEach((el, i) => {
+    el.classList.add(`gallery-item-${i+1}`);
+  });
+}
+
+// Update the current order of the carouselArray and gallery
+setCurrentState(direction) {
+
+  if (direction.className == 'gallery-controls-previous') {
+    this.carouselArray.unshift(this.carouselArray.pop());
+  } else {
+    this.carouselArray.push(this.carouselArray.shift());
+  }
+  
+  this.updateGallery();
+}
+
+// Construct the carousel controls
+setControls() {
+  this.carouselControls.forEach(control => {
+    galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+
+    document.querySelector(`.gallery-controls-${control}`).innerText = control;
+  });
+}
+
+// Add a click event listener to trigger setCurrentState method to rearrange carousel
+useControls() {
+  const triggers = [...galleryControlsContainer.childNodes];
+
+  triggers.forEach(control => {
+    control.addEventListener('click', e => {
+      e.preventDefault();
+
+      if (control.className == 'gallery-controls-add') {
+        const newItem = document.createElement('img');
+        const latestItem = this.carouselArray.length;
+        const latestIndex = this.carouselArray.findIndex(item => item.getAttribute('data-index') == this.carouselArray.length)+1;
+
+        // Assign the necessary properties for new gallery item
+        Object.assign(newItem,{
+          className: 'gallery-item',
+          src: `http://fakeimg.pl/300/?text=${this.carouselArray.length+1}`
+        });
+        newItem.setAttribute('data-index', this.carouselArray.length+1);
+
+        // Then add it to the carouselArray and update the gallery
+        this.carouselArray.splice(latestIndex, 0, newItem);
+        document.querySelector(`[data-index="${latestItem}"]`).after(newItem);
+        this.updateGallery();
+
+      } else {
+        this.setCurrentState(control);
+      }
+
+    });
+  });
+}
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+// exampleCarousel.setNav();
+exampleCarousel.useControls();
